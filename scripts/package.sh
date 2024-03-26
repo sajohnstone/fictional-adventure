c325b93f3c3ad97f8658616958468a3b5c2d8b0f
Stu Johnstone <sajohnstone@yahoo.co.uk>
2024-03-26 12:36:44 +1100

    fix aws install

--- scripts/package.sh
+++ scripts/package.sh
@@ -2,14 +2,10 @@ set -e
 cd ${WORKDIR:-}
 echo "Adding dependancies..."
 apk add zip curl
-
 echo "Install AWS CLI..."
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-./aws/install
+python -m pip install awscli
 echo "Upload packages..."
-/usr/local/bin/aws --version
-/usr/local/bin/aws s3 ls
+aws --version
 echo "Packageing package_layer..."
 mkdir -p ~/.tmp/package_layer
 mkdir -p ~/packages/package_layer/python/common
